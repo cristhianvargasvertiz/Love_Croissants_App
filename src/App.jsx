c6ef0f24eefb9976b84e21e3de5d4402814bed74
Cristhian Vargas <cristhianvar09@gmail.com>
2024-09-29 05:38:06 -0500

    Love_Croissants_App_stage_0

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-
 import React, { useState } from 'react';
 import ProductList from './ProductList';
 import './App.css';
 import AboutUs from './AboutUs';
 
 function App() {
-  
   const [showProductList, setShowProductList] = useState(false);
 
   const handleGetStartedClick = () => {
@@ -13,27 +11,33 @@ function App() {
   };
   const handleReturnClick = () => {
     setShowProductList(false);
-  }
+  };
 
   return (
     <div className="app-container">
-      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
+      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}> {/* comentario: React elimina gradualmente el contenido de landing-page debido a que no existe la  y renderiza el nuevo contenido de product-list-container en su lugar. */}
         <div className="background-image"></div>
         <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-         
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
-         </div>
-          <div className="aboutus_container">
-          <AboutUs/>
+          <div className="landing_content">
+            <img
+              src="/Love_Croissants_App/Logo1.png" // Asegúrate que la imagen esté en public
+              alt="logo1"
+              style={{
+                display: 'block',
+                maxWidth: '100%',
+                height: 'auto',
+                margin: '0 auto',
+                overflow: 'visible',
+              }}
+            />
+            <div className="divider"></div>
+            <p>"Love is hot-from the oven daily 7am-2pm"</p>
+            <button className="get-started-button" onClick={handleGetStartedClick}>
+              Get Started
+            </button>
           </div>
-          </div>
-
+          
+        </div>
       </div>
       <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
         <ProductList toLanding={handleReturnClick} />
